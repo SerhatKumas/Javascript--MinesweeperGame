2d4a16a62395e2518085aeb2d6880f255ea16d69
Serhat Kumas <serhatkumas@outlook.com>
2023-02-13 20:34:13 +0300

    Ui change, game check and game loss cases are implemented

--- src/MinesweeperRunner.js
+++ src/MinesweeperRunner.js
@@ -1,4 +1,4 @@
-let placeArray = []
+let placeArray = null;
 let numberOfMineField = 25;
 
 function gameInitializer(){
@@ -6,7 +6,6 @@ let maxNumberOfMines = 5;
 let numberOfMines = randomNumberCreatorForMines(maxNumberOfMines);
 document.getElementById("mine-num").innerHTML = numberOfMines;
 placeArray = randomPlaceCreatorForMines(numberOfMines);
-console.log(placeArray);
 }
 
 function randomNumberCreatorForMines(maxNumberOfMines){
@@ -23,4 +22,26 @@ function randomPlaceCreatorForMines(numberOfMines){
         } 
     }
     return placeArray;
+}
+
+function checkTheField(id){
+    if(placeArray.includes(parseInt(id))){
+    document.getElementById(id).style.backgroundColor = "red";
+    gameLostActions();
+    }
+    else{
+        document.getElementById(id).style.backgroundColor = "green";
+    }
+}
+
+function gameLostActions(){
+    for(let i = 1; i <= numberOfMineField; i++){
+        document.getElementById(i).disabled = true;
+        if(placeArray.includes(i)){
+            document.getElementById(i).style.backgroundColor = "red";
+        }
+        else{
+            document.getElementById(i).style.backgroundColor = "green";
+        }
+    }
 }
